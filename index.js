f3d2ead61da4bafdc2346520d1d39445c7df5aa2
Hoang An <hoangan@Hoangs-Mac-mini.local>
2019-07-24 20:24:24 +0700

    learn express

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoute = require('./routes/user.route');
+
+var port = 3000;
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
+
+// Routes
+app.get('/', function(req, res) {
+    res.render('index', { 
+        name: 'Hoang An'
+    });
+});
+
+app.use('/users', userRoute);
+
+app.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
