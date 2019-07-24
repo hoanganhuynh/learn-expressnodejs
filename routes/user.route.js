394a34553fa139752412aa8f3b900bf765cb411e
Hoang An <hoangan@Hoangs-Mac-mini.local>
2019-07-24 21:12:16 +0700

    learn validate

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 var controller = require('../controllers/user.controller');
+var validate = require('../validate/user.validate');
 
 router.get('/', controller.index);
 router.get('/search', controller.search);
 router.get('/create', controller.create);
 router.get('/:id', controller.get);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 
 module.exports = router;
