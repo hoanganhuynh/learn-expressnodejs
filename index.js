b1b7628a29a446580d486b2a5645254742e22b68
Hoang An <hoangan@Hoangs-Mac-mini.local>
2019-07-25 12:36:34 +0700

    environment variables learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+// console.log(process.env.SESSION_SECRET);
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -13,7 +17,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(cookieParser('asdf4556677'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.use(express.static('public'));
 
