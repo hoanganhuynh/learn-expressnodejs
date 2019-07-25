43d720978d2485b0fb3ec244a349c59a1d3443c5
Hoang An <hoangan@Hoangs-Mac-mini.local>
2019-07-26 00:25:19 +0700

    sessions learn

---
+++ controllers/cart.controller.js
@@ -0,0 +1,32 @@
+var db = require('../db');
+
+module.exports.addToCart = function(req, res, next) {
+    var productId = req.params.productId;
+    var sessionId = req.signedCookies.sessionId;
+
+    if(!sessionId) {
+        res.redirect('/products');
+        return;
+    }
+
+    var count = db
+    .get('sessions')
+    .find({ id: sessionId })
+    .get('cart.' + productId, 0)
+    .value();
+
+    db.get('sessions')
+    .find({ id: sessionId })
+    .set('cart.' + productId, count + 1)
+    .write();
+
+    // Hien thi so san pham trong gio hang
+    // var countProducts = res.locals.countCart = db
+    // .get("sessions")
+    // .find({ id: sessionId })
+    // .get("cart")
+    // .size()
+    // .value();
+
+    res.redirect('/products');
+};
--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const cookieParser = require('cookie-parser');
 const userRoute = require('./routes/user.route');
 const productRoute = require('./routes/product.route');
 const authRoute = require('./routes/auth.route');
+const cartRoute = require('./routes/cart.route');
 
 var authMiddleware = require('./middlewares/auth.middleware');
+var sessionMiddleware = require('./middlewares/session.middleware');
 
 var port = 3000;
 const app = express();
@@ -19,6 +21,7 @@ app.set('views', './views');
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(sessionMiddleware);
 
 app.use(express.static('public'));
 
@@ -31,6 +34,7 @@ app.get('/', function(req, res) {
 app.use('/products', productRoute);
 app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
+app.use('/cart', cartRoute);
 
 app.listen(port, function() {
     console.log('Server listening on port ' + port);
